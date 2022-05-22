cc848e636deb2e19761472e1f08a8c99fcdd10ed
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-05-22 13:47:28 +0600

    added doctors api

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     const serviceCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
     const userCollection = client.db('doctors_portal').collection('users');
+    const doctorCollection = client.db('doctors_portal').collection('doctors');
 
     app.get('/services', async (req, res) => {
       const query = {};
@@ -126,6 +127,12 @@ async function run() {
       }
       const result = await bookingCollection.insertOne(booking);
       return res.send({ success: true, result });
+    });
+
+    app.post('/doctor', async(req, res) =>{
+      const doctor = req.body;
+      const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
     })
 
   }
